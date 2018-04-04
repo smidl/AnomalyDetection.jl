31ea6fed6234ea7f7a3863694f17960c451e0c3d
pevnak <pevnak@gmail.com>
2018-04-04 13:44:07 +0200

    some fixes of run_parallel

--- experiments/run_parallel.jl
+++ experiments/run_parallel.jl
@@ -3,14 +3,14 @@ using Lazy
 using Iterators
 using FileIO
 # this shoul not be necessary when the AnomalyDetection repo is renamed
-script_abs_path = replace(@__FILE__, split(@__FILE__, "/")[end], "")
-push!(LOAD_PATH, joinpath(script_abs_path, "../src"))
+pkgpath = joinpath(Pkg.dir(),"anomaly_detection/src")
+isdir(pkgpath) && push!(LOAD_PATH,pkgpath)
 using AnomalyDetection
 
 @everywhere begin
 	loda_path = "/mnt/output/data/datasets/numerical"
 	export_path = "/mnt/output/anomaly" #master path where data will be stored
-	include(joinpath(script_abs_path, "parallel_utils.jl"))
+	include(joinpath(Pkg.dir("AnomalyDetection"), "experiments/parallel_utils.jl"))
 end
 
 dpaths = joinpath.(export_path, readdir(export_path))
