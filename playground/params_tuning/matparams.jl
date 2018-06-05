626b65589f838a762d3980f34cb8c53bf9adc941
vit.skvara <vit.skvara@gmail.com>
2018-06-05 15:14:43 +0200

    fixed the output file adress

--- playground/params_tuning/matparams.jl
+++ playground/params_tuning/matparams.jl
@@ -12,7 +12,7 @@ nargs = length(ARGS)
 (nargs>0)? infile = ARGS[1] : error("Specify the input file in the first cl parameter. 
 	Run e.g. as julia matparams.jl /path/to/inputs.mat /path/to/outputs.mat")
 
-(nargs>1)? outfile = ARGS[1] : error("Specify the output file in the second cl parameter. 
+(nargs>1)? outfile = ARGS[2] : error("Specify the output file in the second cl parameter. 
 	Run e.g. as julia matparams.jl /path/to/inputs.mat /path/to/outputs.mat")
 
 auroc, tprvec, fprvec = mat_params(infile, outfile)
